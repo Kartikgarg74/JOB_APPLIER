d197a5ca75eea44ba285f73d7830d24a0cea8fea
Kartik Garg <gargkartik74@gmail.com>
2025-07-26 17:32:24 +0530

    Fix import error in user_service: change from profile_api to relative import

--- apps/user_service/src/main.py
+++ apps/user_service/src/main.py
@@ -14,7 +14,7 @@ from packages.errors.custom_exceptions import JobApplierException
 from packages.utilities.logging_utils import setup_logging
 
 from .user_api import router as user_router
-from profile_api import router as profile_router
+from .profile_api import router as profile_router
 
 setup_logging()
 
